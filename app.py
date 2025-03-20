2e286235d940dd0739bcee003f2626dc9e24887b
Ankit-akumar <as2262011@gmail.com>
2025-03-20 16:32:19 +0530

    Added method 3 to get all failed bots

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ from utils import *
 import os
 from dotenv import load_dotenv
 
+# loading env variables
 load_dotenv()
+
+# setting up logging
+file_logger = setup_logger('debug')
+
+file_logger.debug("Starting script execution...")
 
 pengine_cbort = DB_Connect(**{
     "PUSER":os.getenv('RMS_POSTGRES_USERNAME'),
@@ -30,13 +36,11 @@ iengine_greyorange = DB_Connect(**{
     "PDATABASE":os.getenv('RTP_INFLUX_DATABASE')
 }).openconnection_influx()
 
-execution_method = input("Please enter the execution method: 0 for bot id & 1 for pps_id: ")
+execution_method = input("Please enter the execution method: 0 for bot id & 1 for pps_id & 2 for getting all error bots in past 6 hours : ")
 
 if execution_method == '1':
 
     # CASE 1: When pps ID and timestamp is given
-
-    # iengine_sms = DB_Connect().openconnection_influx_sms()
 
     timestamp=input("Please enter the timestamp in UTC format(yyyy-mm-dd HH:MM): ")
     front_pps_id = input("Please enter the PPS ID: ")
@@ -46,15 +50,21 @@ if execution_method == '1':
     dt = datetime.strptime(timestamp, "%Y-%m-%d %H:%M")
     timestamp_start = dt.replace(second=0).isoformat() + "Z"
     timestamp_end = dt.replace(second=59).isoformat() + "Z"
-    # print(timestamp)
-    # print(timestamp_start)
-    # print(timestamp_end)
+
+    file_logger.debug(f"formatted timestamp influxDB - {timestamp}, timestamp_start - {timestamp_start}, timestamp_end - {timestamp_end}")
 
     failed_arrived_msg = get_failed_arrived_msg_by_ppsID(iengine_greyorange=iengine_greyorange, timestamp_start=timestamp_start, timestamp_end=timestamp_end, front_pps_id=front_pps_id)
 
     points_list = list(failed_arrived_msg.get_points())  # Convert iterator to list
 
-else:
+    if points_list:  
+        execute_by_bot(pengine_cbort=pengine_cbort, points_list=points_list, file_logger=file_logger)
+    else:
+        print()
+        file_logger.error(f"No failed arrival messages found in the past 3 days")
+        print(f"No failed arrival messages found in the past 3 days")
+
+elif execution_method == '0':
     # CASE 2: When bot ID is given
 
     bot_id = input("Please enter the bot id: ")
@@ -64,58 +74,19 @@ else:
 
     points_list = list(failed_arrived_msg.get_points())  # Convert iterator to list
 
-
-# Getting shipments the bot was carrying before arrived event failed
-
-if points_list:  # Check if there is any data
-    first_point = points_list[0]  # First item
-
-    time, back_pps_id, front_pps_id, bin_id, processing_failure_reason = None, None, None, None, None
-
-    for point in points_list:  # Iterate over all points
-        time = point["received_at"]
-        back_pps_id = point["back_pps_id"]
-        front_pps_id = point["front_pps_id"]
-        bin_id = point["bin_id"]
-        bot_id = point["bot_id"]
-        processing_failure_reason = point["processing_failure_reason"]
-
-    print(f"Bot ID {bot_id} sent an unprocessable arrived event at {time} from back PPS {back_pps_id}, front PPS {front_pps_id} and bin {bin_id} with failure reason - {processing_failure_reason}")
-
-    # formatting time for postgres query
-    dt = datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ")
-    formatted_time = dt.strftime("%Y-%m-%d %H:%M:%S")
-    # print(formatted_time)
-
-    # Getting previous shipments sorted by the bot
-    shipments = get_shipments_from_bot(time=formatted_time,pengine_cbort=pengine_cbort,bot_id=bot_id)
-
-    # printing shipments
-    if shipments:
-        print_shipments(shipments=shipments)
-        link_to_sop='https://work.greyorange.com/confluence/x/aWKxDQ'
-        print(f"Kindly send the drop events manually. Here is the link to SOP - {link_to_sop}")
+    if points_list: 
+        execute_by_bot(pengine_cbort=pengine_cbort, points_list=points_list, file_logger=file_logger)
     else:
-        print(f"ERROR: No shipments found for the given bot {bot_id} before {time}")
+        print()
+        file_logger.error(f"No failed arrival messages found in the past 3 days")
+        print(f"No failed arrival messages found in the past 3 days")
 
 else:
-    print(f"No failed arrival messages found in the past 3 days")
+    # CASE3: Getting all failed bots in the past 6 hours
 
+    df = get_all_failed_arrived_msg(iengine_greyorange=iengine_greyorange)
 
-    # # Getting induct ID from induct_pps_mapping
-    # induct_id = get_induct_id(front_pps_id=front_pps_id, pengine_wms_process=pengine_wms_process)
-    # print(f"Induct ID is {induct_id}")
+    get_count_shipments_data_for_bot(pengine_cbort=pengine_cbort, df=df)
 
-    # if induct_id:
-    #         # Getting bot ids that arrived on the induct during the time interval
-    #         arrived_bots = get_arrived_bots(iengine_sms=iengine_sms, timestamp_start=timestamp_start, timestamp_end=timestamp_end, induct_id=induct_id)
+file_logger.debug("Script executed")
 
-    #         points_itr = arrived_bots.get_points()
-    #         first_point = next(points_itr,None)
-
-    #         if first_point:
-    #             for point in arrived_bots.get_points():
-    #                 print(point['bot_id'])
-
-    # else:
-    #     print(f"ERROR: Could not find induct id from induct_pps_mapping for front_pps_id = \'{front_pps_id}\'")
--- utils.py
+++ utils.py
@@ -1,5 +1,35 @@
 from sqlalchemy import text
 from datetime import datetime, timezone
+import logging
+import os
+import pandas as pd
+
+def setup_logger(filename, console=False, level=logging.DEBUG):
+    filename = os.path.dirname(os.path.realpath(__file__)) + '/logs/{}.log'.format(filename)
+    logger = logging.getLogger(filename)
+    logger.setLevel(level)
+    
+    if console:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+
+        #Creating Formatter
+        formatter = logging.Formatter("%(asctime)s %(levelname)s - %(message)s")
+
+        #Adding formatter to Console Handler
+        console_handler.setFormatter(formatter)
+
+        #Adding console handler to logging
+        logger.addHandler(console_handler)
+    
+    else:
+        file = logging.FileHandler(filename)
+        formatter = logging.Formatter("%(asctime)s %(levelname)s - %(message)s")
+
+        file.setFormatter(formatter)
+        logger.addHandler(file)
+
+    return logger
 
 # To get failed arrived messages of a bot from GTP influxDB
 def get_failed_arrived_msg_by_botID(bot_id, iengine_greyorange):
@@ -74,3 +104,95 @@ def get_failed_arrived_msg_by_ppsID(iengine_greyorange, timestamp_start, timesta
     failed_arrived_msg = iengine_greyorange.query(get_failed_arrived_msg)
 
     return failed_arrived_msg
+
+
+# Get all failed arrived messages in the last 6 hours
+def get_all_failed_arrived_msg(iengine_greyorange):
+    query = f'SELECT * FROM \"GreyOrange\".\"autogen\".\"zw_bot_events\" WHERE time > now() -36h AND time < now() AND event = \'arrived\' AND processing_status = \'unprocessable\' order by time'
+    tables = iengine_greyorange.query(query)
+
+    data = []
+    for measurement, table in tables.items():
+        for record in table:
+            data.append({
+                "bot_id": record.get("bot_id"),
+                "time": record.get("received_at"),
+                "front_pps_id": record.get("front_pps_id"),
+                "back_pps_id": record.get("back_pps_id"),
+                "processing_failure_reason": record.get("processing_failure_reason")
+            })
+
+    df = pd.DataFrame(data)
+
+    # Ensure 'time' is in datetime format
+    df["time"] = pd.to_datetime(df["time"])
+
+    # Sort by time in desc order
+    df = df.sort_values(by="time", ascending=False)
+
+    # Get the lastest entry for each bot_id
+    df = df.groupby("bot_id").first().reset_index()
+
+    print(df.to_string())
+
+    return df
+
+
+# Get all shipment data in the df
+def get_count_shipments_data_for_bot(pengine_cbort, df):
+    with pengine_cbort.connect() as connection:
+        for index,row in df.iterrows():
+            bot_id = row["bot_id"]
+            time = row["time"]
+    
+            result = connection.execute(text(f"select count(*) from data_shipment where botid = '{bot_id}' and induct_time > '{time}';"))
+            result = result.fetchone()[0]
+            if(result == 0):
+                print(f"Bot {bot_id} has not sorted any shipments since experiencing failed arrived event at {time}. Kindly check this bot")
+            else:
+                print(f"Bot {bot_id} has sorted {result} shipments after experiencing failed arrived msg at {time}")
+
+
+
+# Execution method by bot
+def execute_by_bot(pengine_cbort, points_list, file_logger):
+    file_logger.debug(f"failed arrived msg -> {points_list}")
+
+    # First item
+    # first_point = points_list[0]  
+
+    time, back_pps_id, front_pps_id, bin_id, processing_failure_reason = None, None, None, None, None
+
+    # Iterate over all points
+    for point in points_list:  
+        time = point["received_at"]
+        back_pps_id = point["back_pps_id"]
+        front_pps_id = point["front_pps_id"]
+        bin_id = point["bin_id"]
+        bot_id = point["bot_id"]
+        processing_failure_reason = point["processing_failure_reason"]
+
+    arrived_failure_output = f"Bot ID {bot_id} sent an unprocessable arrived event at {time} from back PPS {back_pps_id}, front PPS {front_pps_id} and bin {bin_id} with failure reason - {processing_failure_reason}"
+    print(arrived_failure_output)
+    file_logger.debug(arrived_failure_output)
+
+    # formatting time for postgres query
+    dt = datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ")
+    formatted_time = dt.strftime("%Y-%m-%d %H:%M:%S")
+    file_logger.debug(f"formatted_time for postgres query - {formatted_time}")
+
+    # Getting previous shipments sorted by the bot
+    shipments = get_shipments_from_bot(time=formatted_time,pengine_cbort=pengine_cbort,bot_id=bot_id)
+
+    # printing shipments
+    if shipments:
+        print_shipments(shipments=shipments)
+        file_logger.debug(f"shipments inducted on the bot - {shipments}")
+
+        link_to_sop='https://work.greyorange.com/confluence/x/aWKxDQ'
+        print(f"Kindly send the drop events manually. Here is the link to SOP - {link_to_sop}")
+    else:
+        file_logger.error(f"ERROR: No shipments found for the given bot {bot_id} before {time}")
+        print(f"ERROR: No shipments found for the given bot {bot_id} before {time}")
+
+
